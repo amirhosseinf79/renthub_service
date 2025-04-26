b560c881926785b4593ccc7228a4696483597e59
Amirhossein <amirhosseinfarrokhmehr1379@gmail.com>
2025-04-27 03:11:17 +0330

    more solid

--- internal/services/homsa/easy_login.go
+++ internal/services/homsa/easy_login.go
@@ -8,45 +8,52 @@ import (
 	"github.com/amirhosseinf79/renthub_service/internal/services/requests"
 )
 
-func (h *homsaService) EasyLogin(fields dto.ApiEasyLogin) (err error) {
+func (h *homsaService) validateFields(fields dto.ApiEasyLogin) error {
 	if fields.Username == "" || fields.Password == "" {
-		return
+		return dto.ErrEmptyAuth
 	}
+	return nil
+}
 
+func (h *homsaService) performLoginRequest(fields dto.ApiEasyLogin, otp bool) (authResponse *dto.HomsaAuthResponse, err error) {
 	bodyRow := dto.HomsaLoginUserPass{
 		Mobile:   fields.Username,
 		Password: fields.Password,
-		UseOTP:   false,
+		UseOTP:   otp,
 	}
 
 	url := h.apiUrl + h.endpoints.LoginWithPass
 	request := requests.New("POST", url, h.GetHeader(), map[string]string{})
 	err = request.RequestBody(bodyRow)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	request.PrintRequestDump()
 	err = request.CommitRequest()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	if !request.Ok() {
 		var errResponse dto.HomsaErrorResponse
 		err = request.Json(&errResponse)
 		if err != nil {
-			return err
+			return nil, err
 		}
 		err = errors.New(errResponse.Code)
-		return err
+		return nil, err
 	}
 
 	var response dto.HomsaAuthResponse
 	err = request.Json(&response)
 	if err != nil {
-		return err
+		return nil, err
 	}
+	return &response, nil
+}
 
+func (h *homsaService) updateOrCreateAuthRecord(fields dto.ApiEasyLogin, response *dto.HomsaAuthResponse) error {
+	var err error
 	exists := h.apiAuthRepo.CheckExists(fields.UserID, fields.ClientID, h.service)
 	if exists {
 		apiM, err := h.apiAuthRepo.GetByUnique(fields.UserID, fields.ClientID, h.service)
@@ -75,6 +82,22 @@ func (h *homsaService) EasyLogin(fields dto.ApiEasyLogin) (err error) {
 		if err != nil {
 			return err
 		}
+	}
+	return nil
+}
+
+func (h *homsaService) EasyLogin(fields dto.ApiEasyLogin) (err error) {
+	err = h.validateFields(fields)
+	if err != nil {
+		return
+	}
+	response, err := h.performLoginRequest(fields, false)
+	if err != nil {
+		return
+	}
+	err = h.updateOrCreateAuthRecord(fields, response)
+	if err != nil {
+		return
 	}
 	return nil
 }
