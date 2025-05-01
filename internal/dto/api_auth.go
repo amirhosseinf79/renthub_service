49563d949819417aa92ad3604b74696cc295948e
Amirhossein <amirhosseinfarrokhmehr1379@gmail.com>
2025-05-01 13:53:33 +0330

    enhance field validation

--- internal/dto/api_auth.go
+++ internal/dto/api_auth.go
@@ -3,7 +3,7 @@ package dto
 type ApiAuthRequest struct {
 	ClientID     string `json:"clientId" validate:"required"`
 	Username     string `json:"username" validate:"required"`
-	Service      string `json:"service" validate:"required"`
+	Service      string `json:"service" validate:"required,oneof=homsa mihmansho jabama shab jajiga otaghak"`
 	AccessToken  string `json:"token" validate:"required"`
 	Password     string `json:"password"`
 	RefreshToken string `json:"refreshToken"`
