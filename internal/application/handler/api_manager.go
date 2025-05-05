822c8e97bf95b5bcc2cb79a784843001055568b5
Amirhossein <amirhosseinfarrokhmehr1379@gmail.com>
2025-05-06 00:39:54 +0330

    update error response title

--- internal/application/handler/api_manager.go
+++ internal/application/handler/api_manager.go
@@ -146,8 +146,7 @@ func (h *handlerSt) VerifyServiceOTP(ctx fiber.Ctx) error {
 	}, inputBody.Code)
 	if !model.IsSucceed {
 		return ctx.Status(fiber.StatusUnauthorized).JSON(dto.ErrorResponse{
-			Message:        dto.ErrInvalidCode.Error(),
-			ServiceMessage: model.ResponseBody,
+			Message: dto.ErrInvalidCode.Error(),
 		})
 	}
 	return ctx.JSON(h.defaultResponse)
