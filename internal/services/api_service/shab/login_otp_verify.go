1df093b5554c25d443c0d643f1d8842db986c193
Amirhossein <amirhosseinfarrokhmehr1379@gmail.com>
2025-05-06 17:56:06 +0330

    improve otp services

--- internal/services/api_service/shab/login_otp_verify.go
+++ internal/services/api_service/shab/login_otp_verify.go
@@ -8,21 +8,16 @@ import (
 	"github.com/amirhosseinf79/renthub_service/internal/services/requests"
 )
 
-func (h *service) VerifyOtp(fields dto.RequiredFields, otp string) (log *models.Log, err error) {
+func (h *service) VerifyOtp(fields dto.RequiredFields, creds dto.OTPCreds) (log *models.Log, err error) {
 	log = h.initLog(fields.UserID, fields.ClientID)
 	endpoint := h.getEndpoints().LoginSecondStep
-	model, err := h.apiAuthService.GetByUnique(fields.UserID, fields.ClientID, h.service)
-	if err != nil {
-		log.FinalResult = err.Error()
-		return log, err
-	}
 	url, err := h.getFullURL(endpoint)
 	if err != nil {
 		log.FinalResult = err.Error()
 		return log, err
 	}
 	header := h.getHeader()
-	bodyRow := h.generateVerifyOTPBody(model.Username, otp)
+	bodyRow := h.generateVerifyOTPBody(creds.PhoneNumber, creds.OTPCode)
 	request := requests.New(endpoint.Method, url, header, map[string]string{}, log)
 	err = request.Start(bodyRow, endpoint.ContentType)
 	if err != nil {
@@ -30,12 +25,10 @@ func (h *service) VerifyOtp(fields dto.RequiredFields, otp string) (log *models.
 		return log, err
 	}
 	response := h.generateAuthResponse()
-	ok, err := request.Ok()
+	ok, _ := request.Ok()
 	if !ok {
-		log.FinalResult = err.Error()
-		return log, err
+		response = h.generateErrResponse()
 	}
-
 	err = request.ParseInterface(response)
 	if err != nil {
 		log.FinalResult = err.Error()
@@ -49,8 +42,8 @@ func (h *service) VerifyOtp(fields dto.RequiredFields, otp string) (log *models.
 	tokenModel := response.GetToken()
 	tokenfields := dto.ApiAuthRequest{
 		ClientID:     fields.ClientID,
-		Username:     model.Username,
-		Password:     otp,
+		Username:     creds.PhoneNumber,
+		Password:     creds.OTPCode,
 		Service:      h.service,
 		AccessToken:  tokenModel.AccessToken,
 		RefreshToken: tokenModel.RefreshToken,
