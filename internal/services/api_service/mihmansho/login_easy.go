fe6026ceaccfc3cdf951fd9f75750f1adc6766dd
Amirhossein <amirhosseinfarrokhmehr1379@gmail.com>
2025-05-18 11:54:44 +0330

    add auto login for mihmansho

--- internal/services/api_service/mihmansho/login_easy.go
+++ internal/services/api_service/mihmansho/login_easy.go
@@ -1,67 +1,30 @@
 package mihmansho
 
 import (
-	"errors"
-
 	"github.com/amirhosseinf79/renthub_service/internal/domain/models"
 	"github.com/amirhosseinf79/renthub_service/internal/dto"
-	"github.com/amirhosseinf79/renthub_service/internal/services/requests"
 )
 
 func (h *service) AutoLogin(fields dto.RequiredFields) (log *models.Log, err error) {
 	log = h.initLog(fields.UserID, fields.ClientID)
-	endpoint := h.getEndpoints().LoginWithPass
 	model, err := h.apiAuthService.GetByUnique(fields.UserID, fields.ClientID, h.service)
 	if err != nil {
 		log.FinalResult = err.Error()
-		return log, err
+		return
 	}
-	if model.Username == "" || model.Password == "" {
-		err = dto.ErrInvalidCredentials
-		log.FinalResult = err.Error()
-		return log, err
-	}
-	url, err := h.getFullURL(endpoint)
+	newSessionID, err := h.getSession(model.AccessToken)
 	if err != nil {
 		log.FinalResult = err.Error()
-		return log, err
+		return
 	}
-	header := h.getHeader()
-	body := dto.ApiEasyLogin{
-		Username: model.Username,
-		Password: model.Password,
-	}
-	bodyRow := h.generateEasyLoginBody(body)
-	request := requests.New(endpoint.Method, url, header, map[string]string{}, log)
-	err = request.Start(bodyRow, endpoint.ContentType)
-	if err != nil {
-		log.FinalResult = err.Error()
-		return log, err
-	}
-	response := h.generateAuthResponse()
-	ok, _ := request.Ok()
-	if !ok {
-		response = h.generateErrResponse()
-	}
-	err = request.ParseInterface(response)
-	if err != nil {
-		log.FinalResult = err.Error()
-		return log, err
-	}
-	ok, result := response.GetResult()
-	log.FinalResult = result
-	if !ok {
-		return log, errors.New(result)
-	}
-	tokenModel := response.GetToken()
 	tokenfields := dto.ApiAuthRequest{
 		ClientID:     fields.ClientID,
 		Username:     model.Username,
 		Password:     model.Password,
 		Service:      h.service,
-		AccessToken:  tokenModel.AccessToken,
-		RefreshToken: tokenModel.RefreshToken,
-		Ucode:        tokenModel.Ucode,
+		AccessToken:  model.AccessToken,
+		RefreshToken: newSessionID,
+		Ucode:        model.Ucode,
 	}
 	err = h.apiAuthService.UpdateOrCreate(fields.UserID, tokenfields)
 	if err != nil {
